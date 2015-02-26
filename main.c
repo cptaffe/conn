f8ffeb85796be65a4bbf80fe08b8e83af32223c7
Connor Taffe <cpaynetaffe@gmail.com>
2015-02-26 17:27:40 -0600

    update

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 // Copyright (c) 2015 Connor Taffe
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "args.h"
 #include "sock.h"
@@ -30,11 +31,30 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	printf("input unicode hex values (U+XXXXXX)\n");
+
 	// init utf8 parser
-	for(int32_t i = 0; i < 0x10ffff; i++){
-		char str[5] = {0};
-		*((utf8_rune *) str) = utf8_encode(i);
-		printf("%s", (char *) str);
+	size_t len = 10;
+	char *str = malloc(len);
+	size_t slen;
+	while ((slen = getline(&str, &len, stdin)) != -1){
+		// get rid of optional newline
+		for (int i = 0; i < slen; i++) {
+			if (str[i] == '\n') {
+				str[i] = 0;
+				break;
+			}
+		}
+
+		// get numeric input
+		char *end = &str[slen];
+		int32_t i = strtol(str, &end, 16);
+
+		// encode to utf-8
+		char strng[5] = {0};
+		*((utf8_rune *) strng) = utf8_encode(i);
+		printf("got: %#x->%d->'%s'\n", i, i,
+		(*((utf8_rune *) strng) >= 0) ? (char *) strng : "not in utf-8");
 	}
 
 	// respond to specific arguments
@@ -48,6 +68,7 @@ int main(int argc, char *argv[]) {
 		printf("broadcasting to address `%s`\n", a->broadcast);
 		sock_broadcast(a->broadcast, 0xbad, "This is a test.");
 	}
+	free(str);
 
 	// cleanup
 	pa_free(a);
